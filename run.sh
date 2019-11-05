50a1d2034e0677fbada532494bf26f2bc4f6b211
RichardCSantana <richardcsantana@gmail.com>
2019-11-05 13:41:17 -0300

    update java and retry

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ansible-playbook -i localhost -e "@variaveis.json" main-playbook.yml -K
+if [ $1 = "retry" ]
+then
+    echo "retry"
+    ansible-playbook -i localhost -e "@variaveis.json" --limit @/Users/richardsantana/Documents/projetos/ambiente/main-playbook.retry main-playbook.yml -K
+else
+    echo "normal"
+    ansible-playbook -i localhost -e "@variaveis.json" main-playbook.yml -K
+fi
